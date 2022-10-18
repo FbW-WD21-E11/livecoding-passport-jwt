06336c08f9c052b57a7e62731c26150492c670dd
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2022-10-18 11:25:07 +0200

    Added error handling in registration form

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,22 +1,31 @@
 import axios from "axios";
+import { useState } from "react";
 
 export default function Register() {
+  const [error, setError] = useState(false);
+
   const handleFormSubmit = async (event) => {
     event.preventDefault(); // prevent the default browser behaviour
 
     const formData = new FormData(event.target);
 
-    await axios.post("http://localhost:3001/api/users/create", {
-      firstName: formData.get("firstName"),
-      lastName: formData.get("lastName"),
-      userName: formData.get("userName"),
-      email: formData.get("email"),
-      password: formData.get("password"),
-    });
+    try {
+      await axios.post("http://localhost:3001/api/users/create", {
+        firstName: formData.get("firstName"),
+        lastName: formData.get("lastName"),
+        userName: formData.get("userName"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+      });
+      setError(false);
+    } catch (error) {
+      setError(true);
+    }
   };
 
   return (
     <>
+      {error ? <p>Error creating user, please try again</p> : null}
       <form onSubmit={handleFormSubmit}>
         <label>
           First Name
